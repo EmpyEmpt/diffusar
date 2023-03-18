333b051cb2a5b31fb85a7ed4128363e36c24c1cf
EmpyEmpt <79100459+EmpyEmpt@users.noreply.github.com>
2023-03-18 15:30:04 +0400

    Better caching and verbosiry

--- src/data/coco_transform.py
+++ src/data/coco_transform.py
@@ -8,6 +8,7 @@ import yaml
 import sys
 import os
 import argparse
+from tqdm import tqdm
 
 
 def str2bool(arg: str) -> bool:
@@ -40,26 +41,46 @@ def transform_coco(max_amount: int, download: bool = False) -> None:
     """
     data = []
 
-    for idx, i in enumerate(coco.anns):
+    bad_images = []
+    cached_images = []
+
+    pbar = tqdm(coco.anns)
+    for i in pbar:
+        # Load necessary metadata
         image_id = coco.anns[i]['image_id']
         caption = coco.anns[i]['caption']
 
-        print(f'Processing image_id {image_id}...   ', end=' ')
-        # image = coco.imgs[image_id]
+        # print(f'Processing image_id {image_id}...   ', end=' ')
+        pbar.set_description(f'Processing image_id {image_id}')
         url = coco.imgs[image_id]['url']
         file_name = coco.imgs[image_id]['file_name']
 
+        # check if image was processed already
+        if file_name in bad_images:
+            continue
+
+        if file_name in cached_images:
+            data.append({
+                'target': file_name,
+                'prompt': caption,
+                'source': '',
+                'url': url
+            })
+            continue
+
+        # process if wasnt
         if not os.path.isfile(f'{images_dir}/{file_name}'):
             response = requests.get(url)
             if not response.ok:
-                print("Image is not accessible :(")
+                bad_images.append(file_name)
                 continue
 
             if download:
                 img = Image.open(BytesIO(response.content))
                 img.save(f'{images_dir}{file_name}')
 
-        print('COMPLETE')
+            cached_images.append(file_name)
+
         data.append({
             'target': file_name,
             'prompt': caption,
@@ -67,14 +88,15 @@ def transform_coco(max_amount: int, download: bool = False) -> None:
             'url': url
         })
 
-        if idx > max_amount:
+        if len(data) > max_amount:
             break
 
-    print('Sacing data...')
+    print('Saving data...')
     data = {"tags": data}
 
     with open(annotations_path, 'w') as outfile:
         json.dump(data, outfile)
+
     print(f'Done! {len(data["tags"])} image-caption pairs in total')
 
 
