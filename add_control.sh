b672e8a6ec5cc3858c03e59bca5e2499af9c8184
EmpyEmpt <79100459+EmpyEmpt@users.noreply.github.com>
2023-03-28 18:58:21 +0400

    Removed unnecessary files for Palette

--- add_control.sh
+++
@@ -1,3 +0,0 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
-python $SCRIPT_DIR/src/models/tool_add_control_sd21.py $SCRIPT_DIR/models/v2-1-pruned-ema.ckpt $SCRIPT_DIR/models/control_v2-1_ini.ckpt
