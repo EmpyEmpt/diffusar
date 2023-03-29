bbd2424dbf03acbe16a3f407ef46dab02237a572
EmpyEmpt <79100459+EmpyEmpt@users.noreply.github.com>
2023-03-29 19:11:01 +0400

    Rearranged a few things 😼

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -11,41 +11,40 @@ model_config = config['training']['model_config_path']
 model_config = yaml.safe_load(open(model_config))
 
 #   Model args
-paths: dict[str, str] = config['models']
+paths: dict[str, str]           = config['models']
 
 #   Unet
-unet_name: str = model_config['unet']['name']
-unet_args: dict = model_config['unet']['unet_args']
+unet_name: str                  = model_config['unet']['name']
+unet_args: dict                 = model_config['unet']['unet_args']
 
 #   EMA
-ema_scheduler: dict = model_config['ema_scheduler']['ema_scheduler_args']
+ema_scheduler: dict             = model_config['ema_scheduler']['ema_scheduler_args']
 if not model_config['ema_scheduler']['use']:
     ema_scheduler = None
 
-optimizer_name: str = model_config['optimizer']['name']
-optimizer_args: dict = model_config['optimizer']['optimizer_args']
-scheduler_args: dict = model_config['scheduler_args']
+optimizer_name: str             = model_config['optimizer']['name']
+optimizer_args: dict            = model_config['optimizer']['optimizer_args']
+scheduler_args: dict            = model_config['scheduler_args']
 
 # Misc
-seed: int = config['misc']['seed']
+seed: int                       = config['misc']['seed']
 
 # Training
-training_args: dict = config['training']['training_args']
+training_args: dict             = config['training']['training_args']
 
 #   Dataloader
-batch_size: int = config['training']['dataloader']['batch_size']
-dataloader_workers: int = config['training']['dataloader']['dataloader_workers']
+batch_size: int                 = config['training']['dataloader']['batch_size']
+dataloader_workers: int         = config['training']['dataloader']['dataloader_workers']
 
+# Dataset
+images_path: str                = config['dataset']['images_dir']
+annotations_path: str           = config['dataset']['annotations_path']
 
 #   Trainer config
 # trainer_args: dict              = config['training']['trainer']
 
 #   Callbacks
 # ckpt_callback_args: dict        = config['callbacks']['model_checkpoint']
-
-# Dataset
-images_path: str = config['dataset']['images_dir']
-annotations_path: str = config['dataset']['annotations_path']
 
 # Logger callback
 # logger = WandbLogger(
@@ -55,17 +54,17 @@ annotations_path: str = config['dataset']['annotations_path']
 # )
 
 # logger.watch(
-#     model,
+#     model, 
 #     log=config['wandb']['log'],
 #     log_freq=config['wandb']['log_freq']
 # )
-
 
 # Callbacks
 # checkpoint_callback = ModelCheckpoint(
 #     **ckpt_callback_args
 # )
 
+# Trainer
 # trainer = pl.Trainer(
 #     logger=logger,
 #     callbacks=checkpoint_callback,
@@ -76,6 +75,7 @@ annotations_path: str = config['dataset']['annotations_path']
 # trainer.fit(model, dataloader)
 
 
+
 def main():
     # torch.backends.cudnn.enabled = True
     utils.set_seed(seed)
@@ -83,23 +83,21 @@ def main():
     network = utils.get_network(unet_name, unet_args, scheduler_args)
     optimizer = utils.get_optimizer(network, optimizer_name, optimizer_args)
     loss_fn = utils.get_loss_fn()
-    dataloader = utils.get_dataloader(
-        images_path, annotations_path, dataloader_workers, batch_size)
+    dataloader = utils.get_dataloader(images_path, annotations_path, dataloader_workers, batch_size)
 
     model = Palette(
-        network=network,
-        loss=loss_fn,
+        network = network, 
+        loss = loss_fn,
         optimizer=optimizer,
         main_loader=dataloader,
         val_loader=None,
         ema_scheduler=ema_scheduler,
-        phase='train',
+        phase = 'train',
         **training_args,
         **paths
     )
 
     model.train()
-    # model.save_everything()
     # print(model.netG_EMA)
 
 
