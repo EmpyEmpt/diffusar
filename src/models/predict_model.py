e280a91fd3c9bf0d4afb1c92f1402c2b61703361
EmpyEmpt <79100459+EmpyEmpt@users.noreply.github.com>
2023-03-29 23:05:28 +0400

    Draft of prediction script 🦄

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,132 +1,63 @@
-import einops
-import gradio as gr
-import numpy as np
+# TODO: Add Gradio interface
+from torchvision import transforms
+from core.util import tensor2img
 import torch
-import random
-import os
-
 from pytorch_lightning import seed_everything
-from cldm.model import create_model, load_state_dict
-from cldm.ddim_hacked import DDIMSampler
 from PIL import Image
+import utils
 
 import yaml
 config = yaml.safe_load(open('./config.yaml'))
+model_config = config['training']['model_config_path']
+model_config = yaml.safe_load(open(model_config))
 
-inference_model_path: str                = config['models']['control_path']
-if os.path.isfile(config['models']['last_path']):
-    inference_model_path: str            = config['models']['last_path']
+seed_everything(config['misc']['seed'])
 
-cldm_path: str                          = config['models']['cldm_path']
-saving_memory: bool             = config['misc']['saving_memory']
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#   Model args
+paths: dict[str, str]           = config['models']
 
+model_path: str                 = paths['load_path']
+image_path: str                 = "who.cares"
 
-model = create_model(cldm_path).cpu()
-model.load_state_dict(load_state_dict(inference_model_path, location='cuda'))
-model = model.cuda()
-ddim_sampler = DDIMSampler(model)
+#   Unet
+unet_name: str                  = model_config['unet']['name']
+unet_args: dict                 = model_config['unet']['unet_args']
+scheduler_args: dict            = model_config['scheduler_args']
 
+# TODO: It whould be a lot better to change this function to not initialize it for inference
+#   like break it down to 'get_network' and 'init_network'
+model = utils.get_network(unet_name, unet_args, scheduler_args)
 
-def __resize_with_pad(image, desired_size):
-    im = Image.fromarray(image)
-    old_size = im.size
+state_dict = torch.load(model_path)
+model.load_state_dict(state_dict, strict=False).to(device)
+model.set_new_noise_schedule(device = device)
+model.eval()
 
-    ratio = float(desired_size)/max(old_size)
-    new_size = tuple([int(x*ratio) for x in old_size])
+tfs = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+])
 
-    im = im.resize(new_size, Image.Resampling.LANCZOS)
+# Read input image
+img_pillow = Image.open(image_path).convert('RGB')
+cond_image = tfs(img_pillow)
+cond_image = cond_image.to(device)
 
-    new_im = Image.new("RGB", (desired_size, desired_size))
-    new_im.paste(im, ((desired_size-new_size[0])//2,
-                      (desired_size-new_size[1])//2))
+# Inference
+with torch.no_grad():
+    output, visuals = model.restoration(
+        cond_image, 
+        sample_num = 8
+    )
 
-    image = np.array(new_im)
-    return image
+# Save intermediates
+output = output.detach().float().cpu()
+for i in range(visuals.shape[0]):
+    img = tensor2img(visuals[i].detach().float().cpu())
+    Image.fromarray(img).save(f"./result/process_{i}.jpg")
 
-
-def process(input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta):
-    with torch.no_grad():
-        # detected_map = apply_uniformer(
-        #     resize_image(input_image, detect_resolution))
-        img = __resize_with_pad(input_image, image_resolution)
-        H, W, C = img.shape
-
-        # detected_map = cv2.resize(detected_map, (W, H), interpolation=cv2.INTER_NEAREST)
-
-        control = torch.from_numpy(img.copy()).float().cuda() / 255.0
-        control = torch.stack([control for _ in range(num_samples)], dim=0)
-        control = einops.rearrange(control, 'b h w c -> b c h w').clone()
-
-        if seed == -1:
-            seed = random.randint(0, 65535)
-        seed_everything(seed)
-
-        if saving_memory:
-            model.low_vram_shift(is_diffusing=False)
-
-        cond = {"c_concat": [control], "c_crossattn": [
-            model.get_learned_conditioning([prompt + ', ' + a_prompt] * num_samples)]}
-        un_cond = {"c_concat": None if guess_mode else [control], "c_crossattn": [
-            model.get_learned_conditioning([n_prompt] * num_samples)]}
-        shape = (4, H // 8, W // 8)
-
-        if saving_memory:
-            model.low_vram_shift(is_diffusing=True)
-
-        model.control_scales = [strength * (0.825 ** float(12 - i)) for i in range(13)] if guess_mode else (
-            [strength] * 13)  # Magic number. IDK why. Perhaps because 0.825**12<0.01 but 0.826**12>0.01
-        samples, intermediates = ddim_sampler.sample(ddim_steps, num_samples,
-                                                     shape, cond, verbose=False, eta=eta,
-                                                     unconditional_guidance_scale=scale,
-                                                     unconditional_conditioning=un_cond)
-
-        if saving_memory:
-            model.low_vram_shift(is_diffusing=False)
-
-        x_samples = model.decode_first_stage(samples)
-        x_samples = (einops.rearrange(x_samples, 'b c h w -> b h w c')
-                     * 127.5 + 127.5).cpu().numpy().clip(0, 255).astype(np.uint8)
-
-        results = [x_samples[i] for i in range(num_samples)]
-    return [img] + results
-
-
-block = gr.Blocks().queue()
-with block:
-    with gr.Row():
-        gr.Markdown("## Diffusar")
-    with gr.Row():
-        with gr.Column():
-            input_image = gr.Image(source='upload', type="numpy")
-            prompt = gr.Textbox(label="Prompt")
-            run_button = gr.Button(label="Run")
-            with gr.Accordion("Advanced options", open=False):
-                num_samples = gr.Slider(
-                    label="Images", minimum=1, maximum=12, value=1, step=1)
-                image_resolution = gr.Slider(
-                    label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
-                strength = gr.Slider(
-                    label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
-                guess_mode = gr.Checkbox(label='Guess Mode', value=False)
-                detect_resolution = gr.Slider(
-                    label="Segmentation Resolution", minimum=128, maximum=1024, value=512, step=1)
-                ddim_steps = gr.Slider(
-                    label="Steps", minimum=1, maximum=100, value=20, step=1)
-                scale = gr.Slider(label="Guidance Scale",
-                                  minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                seed = gr.Slider(label="Seed", minimum=-1,
-                                 maximum=2147483647, step=1, randomize=True)
-                eta = gr.Number(label="eta (DDIM)", value=0.0)
-                a_prompt = gr.Textbox(
-                    label="Added Prompt", value='best quality, extremely detailed')
-                n_prompt = gr.Textbox(label="Negative Prompt",
-                                      value='ut of frame, lowres, text, error, cropped, worst quality, low quality, jpeg artifacts, ugly, duplicate, morbid, mutilated, out of frame, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, dehydrated, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, username, watermark, signature')
-        with gr.Column():
-            result_gallery = gr.Gallery(
-                label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
-    ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution,
-           detect_resolution, ddim_steps, guess_mode, strength, scale, seed, eta]
-    run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
-
-
-block.launch(server_name='0.0.0.0', share = True)
+# Save output 
+img = tensor2img(output)
+Image.fromarray(img).save("./result/output.jpg")
